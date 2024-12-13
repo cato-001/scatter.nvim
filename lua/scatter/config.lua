7223df448fc4e765ae3644dd129afa70d8bd12bc
Lukas Weber <l.weber@traveltrex.com>
2024-12-13 18:36:30 +0100

    update config default path

--- lua/scatter/config.lua
+++ lua/scatter/config.lua
@@ -1,6 +1,6 @@
 local config = {}
 
-config.path = vim.fn.fnamemodify(vim.fs.normalize('~/scatter'), ':p')
+config.path = vim.fn.fnamemodify(vim.fs.normalize('~/notes'), ':p')
 config.notes_path = vim.fn.fnamemodify(vim.fs.normalize(config.path .. '/notes'), ':p')
 config.carlender_path = vim.fn.fnamemodify(vim.fs.normalize(config.path .. '/carlender'), ':p')
 
